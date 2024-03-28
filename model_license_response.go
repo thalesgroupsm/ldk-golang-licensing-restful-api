e818d4bb7ceae07c2c0ce118a4a0c44c14a56b71
cornor2000 <67540463+cornor2000@users.noreply.github.com>
2024-03-28 13:52:59 +0800

    Update model_license_response.go

--- model_license_response.go
+++ model_license_response.go
@@ -7,11 +7,12 @@
 package ldklicensingretfulapi
 
 type LicenseResponse struct {
-	ErrorCode   int          `json:"errorCode"`
-	SessionId   string       `json:"sessionId,omitempty"`
-	SessionInfo *SessionInfo `json:"sessionInfo,omitempty"`
-	FeatureId   int32        `json:"featureId,omitempty"`
-	ProductId   int32        `json:"productId,omitempty"`
-	KeyId       int64        `json:"keyId,omitempty"`
-	Message     string       `json:"message"`
+	ErrorCode     int          `json:"errorCode"`
+	SessionId     string       `json:"sessionId,omitempty"`
+	SessionInfo   *SessionInfo `json:"sessionInfo,omitempty"`
+	FeatureId     int32        `json:"featureId,omitempty"`
+	ProductId     int32        `json:"productId,omitempty"`
+	KeyId         int64        `json:"keyId,omitempty"`
+	Message       string       `json:"message"`
+	LmAccessToken string       `json:"lmAccessToken"`
 }
